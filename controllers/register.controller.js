9bad4e97d740693631e11c8676f2d9a18ea52745
deepakpanda <deepak.panda@hpe.com>
2016-05-04 14:52:44 +0530

    Update register.controller.js

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -1,8 +1,9 @@
-﻿var express = require('express');
+var express = require('express');
 var router = express.Router();
 var request = require('request');
-var config = require('config.json');
+//var config = require('config.json');
 
+/*
 router.get('/', function (req, res) {
     res.render('register');
 });
@@ -31,6 +32,42 @@ router.post('/', function (req, res) {
         req.session.success = 'Registration successful';
         return res.redirect('/login');
     });
-});
+}); 
 
-module.exports = router;
+module.exports = router;*/
+
+
+
+module.exports = function(parameter) {
+  return {
+    get('/', function (req, res) {
+      res.render('register');
+    }); 
+
+    post('/', function (req, res) {
+    // register using api to maintain clean separation between layers
+    request.post({
+        url: parameter + '/users/register',
+        form: req.body,
+        json: true
+    }, function (error, response, body) {
+        if (error) {
+            return res.render('register', { error: 'An error occurred' });
+        }
+
+        if (response.statusCode !== 200) {
+            return res.render('register', {
+                error: response.body,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                username: req.body.username
+            });
+        }
+
+        // return to login page with success message
+        req.session.success = 'Registration successful';
+        return res.redirect('/login');
+    });
+});
+  }
+}
